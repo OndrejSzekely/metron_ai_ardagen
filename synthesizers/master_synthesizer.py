184682f23625b247c918fc9aa6496c9feeec1600
ondra <ondra.szekely@gmail.com>
2022-07-31 12:13:39 +0200

    feat: Update `Master Synthesizer`

--- synthesizers/master_synthesizer.py
+++ synthesizers/master_synthesizer.py
@@ -4,9 +4,10 @@
 
 
 import importlib
-from omegaconf import OmegaConf, DictConfig
+from typing import Dict, Iterable
+from omegaconf import OmegaConf
 from tools.isaac_sim import IsaacSimApp
-from metron_shared.config.config import GetHydraConfig
+from .synth_workers.base_synthesizer import BaseSynthesizer
 from metron_shared import param_validators as param_val
 
 
@@ -16,23 +17,23 @@ class MasterSynthesizer:  # pylint: disable=too-few-public-methods
 
     Attributes:
         isaac_sim_app (IsaacSimApp): `Isaac Sim App` instance.
-        worker_synthesizers (List[BaseSynthesizer]): List of `Worker Synthesizers`.
+        synthesizers_workers (List[BaseSynthesizer]): List of `Synthesizer Workers`.
+        synthesizers_worker_names (list[str]): List of `Synthesizer Worker` names.
     """
 
-    def __init__(self, isaac_sim: IsaacSimApp) -> None:
+    def __init__(self, isaac_sim: IsaacSimApp, synthesizer_workers: Dict) -> None:
         """
         Args:
             isaac_sim (IsaacSimApp): `Isaac Sim App` instance.
 
-        Returns (None):
         """
         param_val.check_type(isaac_sim, IsaacSimApp)
+        param_val.check_type(synthesizer_workers, Dict)
 
         self.isaac_sim_app = isaac_sim
-        self._instantiate_synthesizer_workers()  # pylint: disable=no-value-for-parameter
+        self._instantiate_synthesizer_workers(synthesizer_workers)  # pylint: disable=no-value-for-parameter
 
-    @GetHydraConfig
-    def _instantiate_synthesizer_workers(self, hydra_config: DictConfig) -> None:
+    def _instantiate_synthesizer_workers(self, synthesizer_workers: Dict) -> None:
         """
         Instantiates all enabled `Synthesizer Workers`. Objects can't be instantiated directly using Hydra's
         `initialize`, because it has to be get rid of each `Synthesizer Worker` config's `enabled` attribute
@@ -40,21 +41,28 @@ class MasterSynthesizer:  # pylint: disable=too-few-public-methods
         be used or not.
 
         Args:
-            hydra_config (DictConfig): GRE2G configuration parameters provided by Hydra's config.
+            synthesizer_workers (Dict): `Synthesizer Workers` dict.
         """
-        ms_synth: OmegaConf = hydra_config.master_synthesizer
+        self.synthesizers_workers = []
+        self.synthesizers_worker_names = []
+        for synth_worker in synthesizer_workers:
+            synth_worker_module_path, _, synth_worker_class_name = synthesizer_workers[
+                synth_worker
+            ].target_class.rpartition(".")
+            synth_worker_module = importlib.import_module(synth_worker_module_path)
+            synth_worker_conf_dict = OmegaConf.to_container(synthesizer_workers[synth_worker])
+            synth_worker_conf_dict.pop("target_class")
+            self.synthesizers_workers.append(
+                getattr(synth_worker_module, synth_worker_class_name)(rep, **synth_worker_conf_dict)
+            )
+            self.isaac_sim_app.update()
+            self.synthesizers_worker_names.append(synth_worker)
 
-        self.worker_synthesizers = []
-        for synth_worker in ms_synth.synthesizer_workers:
-            if ms_synth.synthesizer_workers[synth_worker].enabled:
-                synth_worker_module_path, _, synth_worker_class_name = ms_synth.synthesizer_workers[
-                    synth_worker
-                ].target_class.rpartition(".")
-                synth_worker_module = importlib.import_module(synth_worker_module_path)
-                synth_worker_conf_dict = OmegaConf.to_container(ms_synth.synthesizer_workers[synth_worker])
-                synth_worker_conf_dict.pop("target_class")
-                synth_worker_conf_dict.pop("enabled")
-                self.worker_synthesizers.append(
-                    getattr(synth_worker_module, synth_worker_class_name)(**synth_worker_conf_dict)
-                )
-                self.isaac_sim_app.update()
+    def __iter__(self) -> Iterable[BaseSynthesizer]:
+        """
+        Returns iterator over the `Synthesizer Workers`.
+
+        Returns:
+            Iterable[BaseSynthesizer]: Iterator over the `Synthesizer Workers`.
+        """
+        return iter(self.synthesizers_workers)
