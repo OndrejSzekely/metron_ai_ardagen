1073414d19918da33b21f9f52eeb8453332de690
ondra <ondra.szekely@gmail.com>
2022-07-31 23:48:45 +0200

    fix: Fix `get_cameras` method & add rotation feat

--- tools/single_camera.py
+++ tools/single_camera.py
@@ -12,29 +12,36 @@ class SingleCamera:
 
     Attributes:
         position (List[float]): Camera's position given by 3 numbers in 3D.
+        rotation (List[float]): Camera's rotation given by 3 numbers in 3D.
         clipping_range (List[float]): Clipping range of 3D objects given by tupple (clipping_min, clipping_max).
         cam_resolution (List[int]): Camera resolution.
     """
 
-    def __init__(self, position: List[float], clipping_range: List[float], resolution: List[int]) -> None:
+    def __init__(
+        self, position: List[float], rotation: List[float], clipping_range: List[float], resolution: List[int]
+    ) -> None:
         """
         Init.
 
         Args:
             position (List[float]): Camera's position given by 3 numbers in 3D.
+            rotation (List[float]): Camera's rotation given by 3 numbers in 3D.
             clipping_range (List[float]): Clipping range of 3D objects given by tupple (clipping_min, clipping_max).
             resolution (List[int]): Camera resolution.
         """
         param_val.check_type(position, List[float])
+        param_val.check_type(rotation, List[float])
         param_val.check_type(clipping_range, List[float])
         param_val.check_type(resolution, List[int])
         param_val.check_length_of_list(position, 3)
+        param_val.check_length_of_list(rotation, 3)
         param_val.check_length_of_list(clipping_range, 2)
         param_val.check_length_of_list(resolution, 2)
         param_val.check_parameter_value_in_range(resolution[0], 1, 7680)
         param_val.check_parameter_value_in_range(resolution[1], 1, 4320)
 
         self.position = position
+        self.rotation = rotation
         self.clipping_range = clipping_range
         self.cam_resolution = resolution
 
@@ -49,7 +56,13 @@ class SingleCamera:
         """
         # Isaac Sim app has to be created before modules can be imported, so called in here.
         import omni.replicator.core as rep
+        import omni.usd
 
-        camera = rep.create.camera(position=self.position, clipping_range=tuple(self.clipping_range))
+        camera = rep.create.camera(
+            position=self.position, rotation=self.rotation, clipping_range=tuple(self.clipping_range)
+        )
         render_product = rep.create.render_product(camera, self.cam_resolution)
-        yield [camera], [render_product]
+        stage = omni.usd.get_context().get_stage()
+        yield [
+            stage.GetPrimAtPath(camera.node.get_prim_path()).GetRelationship("inputs:prims").GetTargets()[0].pathString
+        ], [render_product]
